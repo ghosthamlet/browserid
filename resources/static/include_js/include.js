b820208729296b2cff73c5fdabd9e1c923de9e5b
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-12-15 20:11:48 -0700

    partial integration of winchan - works on modern browsers in the happy case

--- resources/static/include_js/include.js
+++ resources/static/include_js/include.js
@@ -1,5 +1,5 @@
 /**
- * Uncompressed source can be found at https://browserid.org/build/include.js
+ * Uncompressed source can be found at https://browserid.org/include.orig.js
  *
  * ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
@@ -39,7 +39,7 @@
 (function() {
   // this is the file that the RP includes to shim in the
   // navigator.id.getVerifiedEmail() function
-  "use strict";
+//  "use strict";
 
   // local embedded copy of jschannel: http://github.com/mozilla/jschannel
   /**
@@ -648,7 +648,7 @@
 
         obj.bind('__ready', onReady);
         setTimeout(function() {
-          postMessage({ method: scopeMethod('__ready'), params: "ping" }, true);
+//          postMessage({ method: scopeMethod('__ready'), params: "ping" }, true);
         }, 0);
 
         return obj;
@@ -656,6 +656,249 @@
     };
   })();
 
+  // local embedded copy of winchan: http://github.com/lloyd/winchan
+  WinChan = (function() {
+    var IFRAME_NAME = "_moz_vep_comm_iframe";
+
+    // a portable addListener implementation
+    function addListener(w, event, cb) {
+      if(w.attachEvent) w.attachEvent('on' + event, cb);
+      else if (w.addEventListener) w.addEventListener(event, cb, false);
+    }
+
+    // a portable removeListener implementation
+    function removeListener(w, event, cb) {
+      if(w.detachEvent) w.detachEvent('on' + event, cb);
+      else if (w.removeEventListener) w.removeEventListener(event, cb, false);
+    }
+
+    // checking for IE8 or above
+    function isInternetExplorer() {
+      var rv = -1; // Return value assumes failure.
+      if (navigator.appName == 'Microsoft Internet Explorer') {
+        var ua = navigator.userAgent;
+        var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+          rv = parseFloat(RegExp.$1);
+      }
+      return rv >= 8;
+    }
+
+    // feature checking to see if this platform is supported at all
+    function isSupported() {
+      return (window.JSON && window.JSON.stringify &&
+              window.JSON.parse && window.postMessage);
+    }
+
+    // given a URL, extract the origin
+    function extractOrigin(url) {
+      var m = /^(https?:\/\/[-_a-zA-Z\.0-9:]+)/.exec(url);
+      if (m) return m[1];
+      return url;
+    }
+
+    if (isInternetExplorer()) {
+      // find the relay iframe in the opener
+      function findRelay() {
+        var loc = window.location;
+        var frames = window.opener.frames;
+        var origin = loc.protocol + '//' + loc.host;
+        for (i = frames.length - 1; i >= 0; i++) {
+          try {
+            if (frames[i].location.href.indexOf(origin) === 0 &&
+                frames[i].name === IFRAME_NAME)
+            {
+              return frames[i];
+            }
+          } catch(e) { }
+        }
+        return;
+      }
+
+      /*  This is how we roll on IE:
+       *  0. user clicks
+       *  1. caller adds relay iframe (served from trusted domain) to DOM
+       *  2. caller opens window (with content from trusted domain)
+       *  3. window on opening adds a listener to 'message'
+       *  4. window on opening finds iframe
+       *  5. window checks if iframe is "loaded" - has a 'doPost' function yet
+       *  5a. if iframe.doPost exists, window uses it to send ready event to caller
+       *  5b. if iframe.doPost doesn't exist, window waits for frame ready
+       *   5bi. once ready, window calls iframe.doPost to send ready event
+       *  6. caller upon reciept of 'ready', sends args
+       */
+      return {
+        open: function(url, relay_url, winopts, arg, cb) {
+          if (!cb) throw "missing required callback argument";
+
+          // sanity check, are url and relay_url the same origin? 
+          var origin = extractOrigin(url);
+          if (origin !== extractOrigin(relay_url)) {
+            setTimeout(function() {
+              cb('invalid arguments: origin of url and relay_url must match');
+            })
+            return;
+          }
+
+          // first we need to add a "relay" iframe to the document that's served
+          // from the target domain.  We can postmessage into a iframe, but not a
+          // window
+          var iframe = document.createElement("iframe");
+          // iframe.setAttribute('name', framename);
+          iframe.setAttribute('src', relay_url);
+          iframe.style.display = "none";
+          iframe.setAttribute('name', IFRAME_NAME);
+          document.body.appendChild(iframe);
+
+          var w = window.open(url, null, winopts); 
+          var req = JSON.stringify({a: 'request', d: arg});
+
+          // cleanup on unload
+          function cleanup() {
+            document.body.removeChild(iframe);
+            if (w) w.close();
+            w = undefined;
+          }
+
+          addListener(window, 'unload', cleanup);
+
+          function onMessage(e) {
+
+            try {
+              var d = JSON.parse(e.data);
+              if (d.a === 'ready') iframe.contentWindow.postMessage(req, origin);
+              else if (d.a === 'error') cb(d.d);
+              else if (d.a === 'response') {
+                removeListener(window, 'message', onMessage);
+                removeListener(window, 'unload', cleanup);
+                cleanup();
+                cb(null, d.d);
+              }
+            } catch(e) { }
+          };
+
+          addListener(window, 'message', onMessage);
+        },
+        onOpen: function(cb) {
+          var o = "*";
+          var theFrame = findRelay();
+          if (!theFrame) throw "can't find relay frame";
+
+          function onMessage(e) {
+            var d;
+            o = e.origin;
+            try {
+              d = JSON.parse(e.data);
+            } catch(e) { }
+            if (cb) cb(o, d.d, function(r) {
+              cb = undefined;
+              theFrame.doPost(JSON.stringify({a: 'response', d: r}), o);
+            });
+          }
+          addListener(theFrame, 'message', onMessage);
+
+          // we cannot post to our parent that we're ready before the iframe
+          // is loaded.
+          try {
+            theFrame.doPost('{"a": "ready"}', o);
+          } catch(e) {
+            addListener(theFrame, 'load', function(e) {
+              theFrame.doPost('{"a": "ready"}', o);
+            });
+          }
+
+          // if window is unloaded and the client hasn't called cb, it's an error
+          addListener(window, 'unload', function() {
+            if (cb) theFrame.doPost(JSON.stringify({
+              a: 'error', d: 'client closed window'
+            }), o);
+            cb = undefined;
+            // explicitly close the window, in case the client is trying to reload or nav
+            try { window.close(); } catch (e) { };
+          });
+        }
+      };
+    } else if (isSupported()) {
+      return {
+        open: function(url, relay_url, winopts, arg, cb) {
+          if (!cb) throw "missing required callback argument";
+
+          // sanity check, are url and relay_url the same origin? 
+          var origin = extractOrigin(url);
+          if (origin !== extractOrigin(relay_url)) {
+            setTimeout(function() {
+              cb('invalid arguments: origin of url and relay_url must match');
+            })
+            return;
+          }
+
+          var w = window.open(url, null, winopts);
+          var req = JSON.stringify({a: 'request', d: arg});
+
+          // cleanup on unload
+          function cleanup() {
+            if (w) w.close();
+            w = undefined;
+          }
+          addListener(window, 'unload', cleanup);
+
+          function onMessage(e) {
+            try {
+              var d = JSON.parse(e.data);
+              if (d.a === 'ready') w.postMessage(req, origin);
+              else if (d.a === 'error') cb(d.d);
+              else if (d.a === 'response') {
+                removeListener(window, 'message', onMessage);
+                removeListener(window, 'unload', cleanup);
+                cleanup();
+                cb(null, d.d);
+              }
+            } catch(e) { }
+          }
+          addListener(window, 'message', onMessage);
+        },
+        onOpen: function(cb) {
+          var o = "*";
+          var parentWin = window.opener;
+          function onMessage(e) {
+            var d;
+            o = e.origin;
+            try {
+              d = JSON.parse(e.data);
+            } catch(e) {
+              // ignore
+            }
+            cb(o, d.d, function(r) {
+              cb = undefined;
+              parentWin.postMessage(JSON.stringify({a: 'response', d: r}), o);
+            });
+          }
+          addListener(window, 'message', onMessage);
+          parentWin.postMessage('{"a": "ready"}', o);
+
+          // if window is unloaded and the client hasn't called cb, it's an error
+          addListener(window, 'unload', function() {
+            if (cb) parentWin.postMessage(JSON.stringify({
+              a: 'error',
+              d: 'client closed window'
+            }), o);
+            cb = undefined;
+            // explicitly close the window, in case the client is trying to reload or nav
+            try { window.close(); } catch (e) { };
+          });
+        }
+      };
+    } else {
+      return {
+        open: function(url, winopts, arg, cb) {
+          setTimeout(function() { cb("unsupported browser"); }, 0);
+        },
+        onOpen: function(cb) {
+          setTimeout(function() { cb("unsupported browser"); }, 0);
+        }
+      };
+    }
+  })();
 
   var BrowserSupport = (function() {
     var win = window,
@@ -748,25 +991,6 @@
     return iframe;
   }
 
-  function _get_relayframe_id() {
-    var randomString = '';
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (var i=0; i < 8; i++) {
-      randomString += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return randomString;
-  }
-
-  function _open_relayframe(framename) {
-    var doc = window.document;
-    var iframe = doc.createElement("iframe");
-    iframe.setAttribute('name', framename);
-    iframe.setAttribute('src', ipServer + "/relay");
-    iframe.style.display = "none";
-    doc.body.appendChild(iframe);
-
-    return iframe;
-  }
 
   function _open_window(url, name_suffix) {
     url = url || "about:blank";
@@ -824,19 +1048,6 @@
 
     // keep track of these so that we can re-use/re-focus an already open window.
     navigator.id.get = function(callback, options) {
-      var doc = document;
-      function cleanup() {
-        // we no longer remove the relay chan and iframe
-        // since those live for as long as the page does
-
-        if (w) {
-          w.close();
-          w = null;
-        }
-
-        _detatch_event(window, 'unload', cleanup);
-      }
-
       if (typeof callback !== 'function') throw "navigator.id.get() requires a callback argument";
 
       if (options && options.silent) {
@@ -846,41 +1057,26 @@
           callback(null);
         });
       } else {
-        if (w) {
-          // if there is already a window open, just focus the old window.
-          w.focus();
-          return;
-        }
+        // XXX: focus an existing window?
 
         if (!BrowserSupport.isSupported()) {
           w = _open_window(ipServer + "/unsupported_dialog");
           return;
         }
 
-        // we now have the relay iframe and channel already
-        // we just message it.
-        relay_chan.call({
-          method: "get",
-          params: options,
-          success: function(rv) {
-            if (callback) {
-              // return the string representation of the JWT, the client is responsible for unpacking it.
-              callback(rv);
-            }
-            cleanup();
+        WinChan.open(
+          ipServer + '/sign_in',
+          ipServer + '/relay',
+          "menubar=0,location=0,resizable=0,scrollbars=0,status=0,dialog=1,width=700,height=375",
+          {
+            method: "get",
+            params: options
           },
-          error: function(code, msg) {
-            // XXX: we don't make the code and msg available to the user.
-            if (callback) callback(null);
-            cleanup();
+          function(err, r) {
+            // ignore err!
+            callback(r);
           }
-        });
-
-        // open the window now that all else is ready
-        w = _open_window(ipServer + "/sign_in#" + relay_iframe.getAttribute('name'));
-
-        // if the RP window closes, close the dialog as well.
-        _attach_event(window, 'unload', cleanup);
+        );
       }
     };
 
@@ -926,28 +1122,6 @@
         }
       });
     };
-
-    // set up the relay iframe
-    var setup_relay_iframe = function() {
-      if (document.body) {
-        var frameid = _get_relayframe_id();
-        // removed browserid_relay_ prefix, cause why?
-        relay_iframe = _open_relayframe(frameid);
-        relay_chan = Channel.build({
-          window: relay_iframe.contentWindow,
-          origin: ipServer,
-          scope: "mozid"
-        });
-
-      } else {
-        window.setTimeout(setup_relay_iframe, 100);
-      }
-    };
-
-    // wait for onload
-    // _attach_event(window, 'load', setup_relay_iframe);
-    // that didn't work at fixing the issue, so reverting
-    window.setTimeout(setup_relay_iframe, 0);
 
     navigator.id._getVerifiedEmailIsShimmed = true;
   }
